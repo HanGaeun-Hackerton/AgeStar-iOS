2b2133c56d7642d00f769f73d4d318121df00bac
hsj._.06 <gtw030488@gmail.com>
2023-07-04 16:14:59 +0900

    init :: xcode

---
+++ AgeStar-iOS/AgeStar-iOS/AgeStar_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgeStar_iOSApp.swift
+//  AgeStar-iOS
+//
+//  Created by 홍승재 on 2023/07/04.
+//
+
+import SwiftUI
+
+@main
+struct AgeStar_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
